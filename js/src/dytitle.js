6f830cfab7484fd0e4b6e351080334ae0d277fe6
zhaochunyu <chunyu.zhao@duodian.com>
2018-09-20 15:43:00 +0800

    Site updated: 2018-09-20 15:43:00

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -3,12 +3,10 @@ var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
         document.title = ' ヽ(`З’)ﾉ！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = ' (●ﾟωﾟ●) ';
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
